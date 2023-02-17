5b027d623500922a9d9694c7274a0507e6985564
Joakim Sundman <82515307+jayhyllie@users.noreply.github.com>
2023-02-17 15:42:27 +0100

    updated fetch json file

--- package/js/main.js
+++ package/js/main.js
@@ -1,3 +1,5 @@
+import { createPlanet } from "./modules/destination.js";
+
 const primaryNav = document.querySelector('.primary__nav');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
@@ -12,3 +14,26 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false)
     }
 })
+
+async function getDestinations() {
+    let response = await fetch('data.json');
+    let data = await response.json();
+
+    let destinations = data.destinations;
+    const planets = document.querySelectorAll('.destination ul');
+    const planetsLi = document.querySelectorAll('.destination .main__section--list-planet');
+    
+    planets.forEach(planet => {
+        planet.addEventListener('click', function (e) {
+            createPlanet(e, destinations);
+        })
+    })
+
+    planetsLi.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            planetsLi.forEach(p => p.classList.remove('active'));
+            e.target.classList.toggle("active");
+        });
+    });
+}
+getDestinations();
--- package/js/modules/destination.js
+++ package/js/modules/destination.js
@@ -1,71 +1,22 @@
-const planetList = [
-    {
-        title: 'Moon',
-        image: '/package/assets/destination/image-moon.webp',
-        info: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
-        distance: '384,400 KM',
-        travel: '3 days'
-    },
-    {
-        title: 'Mars',
-        image: '/package/assets/destination/image-mars.webp',
-        info: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
-        distance: '225 MIL. KM',
-        travel: '9 Months'
-    },
-    {
-        title: 'Europa',
-        image: '/package/assets/destination/image-europa.webp',
-        info: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
-        distance: '628 MIL. KM',
-        travel: '3 years'
-    },
-    {
-        title: 'Titan',
-        image: '/package/assets/destination/image-titan.webp',
-        info: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
-        distance: '1.6 BIL. KM',
-        travel: '7 years'
-    }
-];
-
-const planets = document.querySelectorAll('.destination ul');
-const planetsLi = document.querySelectorAll('.destination .main__section--list-planet');
 const planetImage = document.querySelector('#destination--image');
 const planetName = document.querySelector('.destination .main__section--head');
 const planetInfo = document.querySelector('.destination .main__section--text');
 const planetDistance = document.querySelector('.destination .info--distance h5');
 const planetTravel = document.querySelector('.destination .info--travel h5');
 
-const DATA_LINK = 'data.json';
+function createPlanet(e, destinations) {
+    let clickedLi = e.target;
+    let clickedTitle = clickedLi.textContent;
+    const clickedBody = destinations.find(body => body.name.toLowerCase() === clickedTitle.toLowerCase());
 
-async function getDestination() {
-    let response = await fetch(DATA_LINK);
-    let data = await response.json();
-
-    let dests = data.destinations;
-    console.log(dests);
-
-    planets.forEach(planet => {
-        planet.addEventListener('click', function (e) {
-            let clickedLi = e.target;
-            let clickedTitle = clickedLi.textContent;
-            const clickedBody = planetList.find(body => body.title.toLowerCase() === clickedTitle.toLowerCase());
-    
-            planetImage.setAttribute('src', clickedBody.image);
-            planetName.innerHTML = clickedBody.title;
-            planetInfo.innerHTML = clickedBody.info;
-            planetDistance.innerHTML = clickedBody.distance;
-            planetTravel.innerHTML = clickedBody.travel;
-        })
-    })
-    planetsLi.forEach((btn) => {
-        btn.addEventListener("click", (e) => {
-            planetsLi.forEach(p => p.classList.remove('active'));
-            e.target.classList.toggle("active");
-        });
-    });
+    planetImage.setAttribute('src', clickedBody.images.webp);
+    planetName.innerHTML = clickedBody.name;
+    planetInfo.innerHTML = clickedBody.description;
+    planetDistance.innerHTML = clickedBody.distance;
+    planetTravel.innerHTML = clickedBody.travel;
 }
-getDestination();
+
+export { createPlanet }
+
 
 
